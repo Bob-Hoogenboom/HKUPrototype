6fdf9ae997d43ff504756452e170c77ac801867a
Bob Hoogenboom <28602@ma-web.nl>
2022-11-14 13:06:54 +0100

    sticky movement script

---
+++ HKU_Katamari _Damacy/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    [SerializeField] private float rollSpeed;
+
+    private Rigidbody _rb;
+
+    void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
